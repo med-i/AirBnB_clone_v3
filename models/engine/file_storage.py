737f231cdf45fd74854ecc67d9a0c80435c0cdc2
Ouazane <mohamedouazane@yahoo.com>
2024-02-03 13:19:44 -0800

    sorting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,6 @@
 Contains the FileStorage class
 """
 
-import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -11,6 +10,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+import json
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
