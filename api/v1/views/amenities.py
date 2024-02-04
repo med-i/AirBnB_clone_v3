8303b92fedf0e4d09e4934cc67ba67eb4cf34ed0
Ouazane <mohamedouazane@yahoo.com>
2024-02-04 04:03:51 -0800

    edit amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -9,49 +9,58 @@ from flask import abort, jsonify, request
 import json
 
 
-@app_views.route('/amenities', methods=['GET'])
+@app_views.route('/amenities', methods=['GET'],
+                 strict_slashes=False)
 def all_amenities():
+    """ Get all amenities """
     amenities_dict = storage.all(Amenity)
     amenities_list = [amenity.to_dict() for amenity in amenities_dict.values()]
-    return amenities_list
+    return jsonify(amenities_list)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def amenities_id(amenity_id):
+    """ Get amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity:
-        return amenity.to_dict(), 201
+        return jsonify(amenity.to_dict()), 201
     else:
         abort(404)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def amenities_delete(amenity_id):
+    """ Delete Amenity """
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
-    storage.delete(amenity)
+    amenity.delete()
     storage.save()
     return jsonify('{}'), 201
 
 
-@app_views.route('/amenities', methods=['POST'])
+@app_views.route('/amenities', methods=['POST'],
+                 strict_slashes=False)
 def amenities_post():
+    """ Add Amenity """
     try:
         data = request.get_data()
         data_object = json.loads(data.decode('utf-8'))
         if 'name' not in data_object:
             abort(400, 'Missing name')
         new_amenity = Amenity(**data_object)
-        storage.new(new_amenity)
         storage.save()
     except json.JSONDecodeError:
         abort(400, 'Not a JSON')
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def amenities_put(amenity_id):
+    """ Update Amenity """
     try:
         amenity_up = storage.get(Amenity, amenity_id)
         if not amenity_up:
