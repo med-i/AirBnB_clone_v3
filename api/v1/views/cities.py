7a148fd8bfa78978e0138845b9b5ecd730dedec7
Ouazane <mohamedouazane@yahoo.com>
2024-02-04 03:54:40 -0800

    edit city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,18 +10,19 @@ from flask import abort, jsonify, request
 import json
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def all_cities(state_id):
     city_list = []
     city_dict = storage.all(City)
-    '''city_list = [city.to_dict() for city in city_dict.values()]'''
     for city in city_dict.values():
         if state_id == city.state_id:
             city_list.append(city.to_dict())
     return jsonify(city_list)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>', methods=['GET'],
+                 strict_slashes=False)
 def cities_id(city_id):
     city = storage.get(City, city_id)
     if not city:
@@ -29,17 +30,19 @@ def cities_id(city_id):
     return jsonify(city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def cities_delete(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    storage.delete(city)
+    city.delete()
     storage.save()
     return jsonify('{}'), 201
 
 
-@app_views.route('states/<state_id>/cities', methods=['POST'])
+@app_views.route('states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def cities_post(state_id):
     try:
         data = request.get_data()
@@ -51,14 +54,14 @@ def cities_post(state_id):
         if 'name' not in data_object:
             abort(400, 'Missing name')
         new_city = City(**data_object)
-        storage.new(new_city)
         storage.save()
     except json.JSONDecodeError:
         abort(400, 'Not a JSON')
     return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route('cities/<city_id>', methods=['PUT'])
+@app_views.route('cities/<city_id>', methods=['PUT'],
+                 strict_slashes=False)
 def cities_put(city_id):
     try:
         city_up = storage.get(City, city_id)
@@ -67,7 +70,7 @@ def cities_put(city_id):
         data = request.get_data()
         data_object = json.loads(data.decode('utf-8'))
         for key, value in data_object.items():
-            if key not in ['id', 'created_at', 'updated_at']:
+            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
                 setattr(city_up, key, value)
         storage.save()
     except json.JSONDecodeError:
